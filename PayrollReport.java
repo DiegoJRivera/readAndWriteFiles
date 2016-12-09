4553b25140b49bb75fd544238e5c0ff12c1dad1f
DiegoJRivera <DiegoJRivera@users.noreply.github.com>
2016-12-09 12:50:01 +1100

    Update PayrollReport.java

--- PayrollReport.java
+++ PayrollReport.java
@@ -1,7 +1,7 @@
 /*
  * Practical Exercise 3 developed by Diego Rivera for Programming Fundamentals class.
  * 25/08/2016
- * Professor Rachel Jeikishore
+ * Professor ----------
  * This program reads data from the supplied text file (Employees.txt) that contains a list of employee details including
  * their monthly pay. The program calculates payroll summary information and display this data on the monitor and also write
  * it to a textfile (PayrollReport.txt).
